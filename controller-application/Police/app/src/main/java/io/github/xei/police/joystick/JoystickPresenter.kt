bb49229d61086d7eda571a5e40bb309c1e94d29e
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2018-03-24 00:24:00 +0430

    complete JoystickPresenter class.

---
+++ controller-application/Police/app/src/main/java/io/github/xei/police/joystick/JoystickPresenter.kt
@@ -0,0 +1,16 @@
+package io.github.xei.police.joystick
+
+/**
+ * Created by hamidreza on 3/23/18.
+ */
+class JoystickPresenter(private val model: JoystickContract.Model, private val view: JoystickContract.View) : JoystickContract.Presenter {
+
+    init {
+        model.presenter = this
+        view.presenter = this
+    }
+
+    override fun start() {
+
+    }
+}
