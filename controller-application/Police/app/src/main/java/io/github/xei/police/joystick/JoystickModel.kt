84a5bca867edd392c3e8d36b3f8f0833e8e5ccc1
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2018-03-24 00:19:16 +0430

    complete JoystickModel class.

---
+++ controller-application/Police/app/src/main/java/io/github/xei/police/joystick/JoystickModel.kt
@@ -0,0 +1,8 @@
+package io.github.xei.police.joystick
+
+/**
+ * Created by hamidreza on 3/23/18.
+ */
+class JoystickModel : JoystickContract.Model {
+    override lateinit var presenter: JoystickContract.Presenter
+}
