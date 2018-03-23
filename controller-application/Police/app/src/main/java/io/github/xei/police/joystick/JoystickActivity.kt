204a687e555dc159b0217f605048f93f4128aa0f
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2018-03-24 00:17:44 +0430

    complete JoystickActivity as a controller for MVP.

--- controller-application/Police/app/src/main/java/io/github/xei/police/joystick/JoystickActivity.kt
+++ controller-application/Police/app/src/main/java/io/github/xei/police/joystick/JoystickActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import io.github.xei.police.R
+import io.github.xei.police.util.ext.replaceFragmentInActivity
 
 class JoystickActivity : AppCompatActivity() {
 
@@ -18,5 +19,17 @@ class JoystickActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_joystick)
+
+        initMVP()
     }
+
+    private fun initMVP() {
+        val model = JoystickModel()
+        val view = supportFragmentManager.findFragmentById(R.id.activityJoystick_frameLayout_fragmentContainer) as JoystickFragment?
+                ?: JoystickFragment.newInstance().also {
+            replaceFragmentInActivity(it, R.id.activityJoystick_frameLayout_fragmentContainer)
+        }
+        JoystickPresenter(model, view)
+    }
+
 }
