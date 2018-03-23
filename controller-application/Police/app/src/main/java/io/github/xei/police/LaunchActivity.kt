a57d8fc6bfcdeeb127733ce39fd31bfa24da9fb1
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2018-03-23 18:58:13 +0430

    Temporarily make Joystick activity launcher.

--- controller-application/Police/app/src/main/java/io/github/xei/police/LaunchActivity.kt
+++ controller-application/Police/app/src/main/java/io/github/xei/police/LaunchActivity.kt
@@ -2,11 +2,15 @@ package io.github.xei.police
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.github.xei.police.joystick.JoystickActivity
 
 class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+
+        // TODO: temporarily open the Joystick activity
+        JoystickActivity.start(this)
     }
 }
