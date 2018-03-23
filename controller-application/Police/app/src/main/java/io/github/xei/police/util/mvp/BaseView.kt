fde715e1836654cfaae814b34e9e7e3c8cf7b394
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2018-03-23 18:39:59 +0430

    add BaseView interface for MVP architectural pattern.

---
+++ controller-application/Police/app/src/main/java/io/github/xei/police/util/mvp/BaseView.kt
@@ -0,0 +1,15 @@
+package io.github.xei.police.util.mvp
+
+import android.view.View
+
+/**
+ * Created by hamidreza on 3/23/18.
+ */
+interface BaseView<T> {
+
+    var presenter: T
+    val isActive: Boolean
+
+    fun findViews(rootView: View)
+    fun setOnClickListeners()
+}
