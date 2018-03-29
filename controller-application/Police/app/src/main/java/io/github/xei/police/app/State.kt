8dc56c27019f145d9c26e628ca8ac3b6930d5e7f
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2018-03-29 23:55:55 +0430

    add sensor State entity.

---
+++ controller-application/Police/app/src/main/java/io/github/xei/police/app/State.kt
@@ -0,0 +1,34 @@
+package io.github.xei.police.app
+
+import com.google.gson.annotations.SerializedName
+
+class State {
+
+    @SerializedName("a0")
+    var a0: Float = 0.toFloat()
+
+    @SerializedName("a1")
+    var a1: Float = 0.toFloat()
+
+    @SerializedName("a2")
+    var a2: Float = 0.toFloat()
+
+    @SerializedName("a3")
+    var a3: Float = 0.toFloat()
+
+    @SerializedName("a4")
+    var a4: Float = 0.toFloat()
+
+    @SerializedName("a5")
+    var a5: Float = 0.toFloat()
+
+    @SerializedName("d2")
+    var d2: Int = 0
+
+    @SerializedName("d4")
+    var d4: Int = 0
+
+    @SerializedName("d7")
+    var d7: Int = 0
+
+}
